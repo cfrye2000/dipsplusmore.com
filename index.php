145de9ae548e766aa8f7004a9ab03bd459c1f27c
Chris Frye <chris.frye@gmail.com>
2011-03-06 13:43:09 -0600

    fixing paths

--- index.php
+++ index.php
@@ -7,24 +7,24 @@
  */
  
  // Required Components
- require_once('../dipsplusmore.com/config.php'); 
- require_once('../dipsplusmore.com/models/Front.php');
- require_once('../dipsplusmore.com/models/view.php');
- require_once('../dipsplusmore.com/models/icontroller.php');
- require_once('../dipsplusmore.com/models/EventUtility.php');
- require_once('../dipsplusmore.com/models/SessionFactory.php');
- require_once('../dipsplusmore.com/models/CartFactory.php');
+ require_once('./config.php'); 
+ require_once('./models/Front.php');
+ require_once('./models/view.php');
+ require_once('./models/icontroller.php');
+ require_once('./models/EventUtility.php');
+ require_once('./models/SessionFactory.php');
+ require_once('./models/CartFactory.php');
  
  // Required Controllers
- require_once('../dipsplusmore.com/controllers/events.php');
- require_once('../dipsplusmore.com/controllers/products.php');
- require_once('../dipsplusmore.com/controllers/adminevents.php');
- require_once('../dipsplusmore.com/controllers/adminproducts.php');
- require_once('../dipsplusmore.com/controllers/admincategories.php');
- require_once('../dipsplusmore.com/controllers/cart.php');
- require_once('../dipsplusmore.com/controllers/login.php');
- require_once('../dipsplusmore.com/controllers/logout.php');
- require_once('../dipsplusmore.com/controllers/account.php');
+ require_once('./controllers/events.php');
+ require_once('./controllers/products.php');
+ require_once('./controllers/adminevents.php');
+ require_once('./controllers/adminproducts.php');
+ require_once('./controllers/admincategories.php');
+ require_once('./controllers/cart.php');
+ require_once('./controllers/login.php');
+ require_once('./controllers/logout.php');
+ require_once('./controllers/account.php');
  
  //Initialize the FrontController
  
